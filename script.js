badcb44759cc8a1ae703e4d25912420e18f52091
umarvv <a_scm@icloud.com>
2025-06-11 00:04:32 +0500

    template added

---
+++ script.js
@@ -0,0 +1,22 @@
+import { products } from "./data.js";
+console.log(1, products);
+
+const template = document.querySelector("template");
+const productList = document.querySelector("#product-list");
+
+products.forEach((product) => {
+  const clone = template.content.cloneNode(true);
+  const cardImage = clone.querySelector(".card-image");
+  const cardTitle = clone.querySelector(".card-title");
+  const rating = clone.querySelector(".rating");
+  const description = clone.querySelector(".description");
+  const discountPrice = clone.querySelector(".discount-price");
+
+  cardTitle.textContent = product.title;
+  description.textContent = product.description;
+  cardImage.src = product.thumbnail;
+  rating.textContent = `${product.rating} ★`;
+  discountPrice.textContent = product.price + " so'm";
+
+  productList.appendChild(clone);
+});
